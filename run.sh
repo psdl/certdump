fe8f21d2f4887116511916c8ba37519a9ea5fffd
psdl <peter.seidl@cksum.de>
2019-09-29 17:46:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 function dump() {
     echo "$(date) Dumping certificates"
-
+    
+    cd /
     dumpcerts.sh /traefik/acme.json /traefik/ssl/
     ln -f /traefik/ssl/certs/* /traefik/ssl/
     ln -f /traefik/ssl/private/* /traefik/ssl/
