5b667061b80f9a200b9c3a38a83419c7d046ad91
psdl <peter.seidl@cksum.de>
2019-09-29 16:24:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,8 +8,6 @@ function dump() {
     ln -f /traefik/ssl/private/* /traefik/ssl/; \
 }
 
-apk --no-cache add inotify-tools jq openssl util-linux bash && \
-wget https://raw.githubusercontent.com/containous/traefik/v1.7/contrib/scripts/dumpcerts.sh -O dumpcerts.sh && \
 mkdir -p /traefik/ssl/ && \
 dump
 
